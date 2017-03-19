2b5ecba08d8da1d2c2fa4d4cd0440cab685e63f2
Ronaiza Cardoso <ronaizacard@gmail.com>
2017-03-19 01:48:03 -0300

    Adding a config example to project

---
+++ config.exemple.js
@@ -0,0 +1,7 @@
+'use strict'
+
+process.env.TELEGRAM_KEY = process.env.TELEGRAM_KEY || <your-bot-api-key>
+
+module.exports = {
+  TELEGRAM_KEY: process.env.TELEGRAM_KEY
+}
--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const Telegram              = require('telegram-node-bot')
 const TodoController        = require('./controllers/todo.js')
 const OtherwiseController   = require('./controllers/otherwise.js')
+const { TELEGRAM_KEY }      = require('./config.js')
+
 const todoController        = new TodoController()
 
-const tg = new Telegram.Telegram('339111967:AAGUe5SMy6yRrtGxlrs8pp30CwzY0836zyo', {
+const tg = new Telegram.Telegram(TELEGRAM_KEY, {
   workers: 1
 })
 
